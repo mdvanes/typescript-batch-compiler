4ced765c203860a4f9b3c744b9552fd21952ea86
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2017-11-05 10:32:13 +0100

    Unit tests - prevent unnecessary duplicate build

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,6 +5,7 @@ const { exec } = require('child_process');
 
 exports.typescriptBatchCompiler = {
     setUp: function (done) {
+        // run before each test
         del(['test/fixtures/*.js', 'test/fixtures/*.js.map'])
             .then(paths => {
                 console.log('Deleted files and folders:\n', paths.join('\n'));
@@ -24,20 +25,24 @@ exports.typescriptBatchCompiler = {
                 done();
             });
     },
-    firstExampleFile: function(test) {
+    'both files created at once': function(test) {
         // Don't filter out linebreaks here, because in that case you can't test for linebreaks in the correct place.
         // If the test fails because of linebreaks, check if the "expected" fixture is saved with LF linebreaks only, not CRLF.
-        const actual = fs.readFileSync(path.join(__dirname, 'fixtures/example1.js'), {encoding: 'utf-8'});
-        const expected = fs.readFileSync(path.join(__dirname, 'expected/example1.js'), {encoding: 'utf-8'});
+        let actual = fs.readFileSync(path.join(__dirname, 'fixtures/example1.js'), {encoding: 'utf-8'});
+        let expected = fs.readFileSync(path.join(__dirname, 'expected/example1.js'), {encoding: 'utf-8'});
         //console.log(actual, actual);
-        test.equal(actual, expected, 'should compile example1');
+        test.equal(actual, expected, 'should have compiled example1');
+
+        actual = fs.readFileSync(path.join(__dirname, 'fixtures/example2.js'), {encoding: 'utf-8'});
+        expected = fs.readFileSync(path.join(__dirname, 'expected/example2.js'), {encoding: 'utf-8'});
+        test.equal(actual, expected, 'should have compiled example2');
 
         test.done();
     },
-    secondExampleFile: function(test) {
-        const actual = fs.readFileSync(path.join(__dirname, 'fixtures/example2.js'), {encoding: 'utf-8'});
-        const expected = fs.readFileSync(path.join(__dirname, 'expected/example2.js'), {encoding: 'utf-8'});
-        test.equal(actual, expected, 'should compile example2');
-        test.done();
-    }
+    // secondExampleFile: function(test) {
+    //     const actual = fs.readFileSync(path.join(__dirname, 'fixtures/example2.js'), {encoding: 'utf-8'});
+    //     const expected = fs.readFileSync(path.join(__dirname, 'expected/example2.js'), {encoding: 'utf-8'});
+    //     test.equal(actual, expected, 'should compile example2');
+    //     test.done();
+    // }
 };
